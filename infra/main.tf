61893b9ca2f03127365add23cadf8550f4fa5149
Florencio Reyes <eldonflor@gmail.com>
2025-07-15 10:07:27 -0600

    EC2 Working with Docker

--- infra/main.tf
+++ infra/main.tf
@@ -74,11 +74,11 @@ resource "aws_security_group" "mysql_sg" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "MySQL"
-    from_port        = 3306
-    to_port          = 3306
-    protocol         = "tcp"
-    cidr_blocks      = ["10.0.1.0/24"]
+    description = "MySQL"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.1.0/24"]
   }
 
   egress {
@@ -104,16 +104,16 @@ resource "aws_db_subnet_group" "mysql_subnet_group" {
 
 
 resource "aws_db_instance" "mysql" {
-  identifier         = "${var.project}-mysql"
-  allocated_storage  = 20
-  engine             = "mysql"
-  engine_version     = "8.0"
-  instance_class     = "db.t3.micro"
-  db_name               = "mydb"
-  username           = var.db_username
-  password           = var.db_password
-  skip_final_snapshot = true
-  publicly_accessible = false
+  identifier             = "${var.project}-mysql"
+  allocated_storage      = 20
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  db_name                = "mydb"
+  username               = var.db_username
+  password               = var.db_password
+  skip_final_snapshot    = true
+  publicly_accessible    = false
   vpc_security_group_ids = [aws_security_group.mysql_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.mysql_subnet_group.name
 
@@ -156,17 +156,17 @@ resource "aws_security_group" "ec2_sg" {
 }
 
 resource "aws_key_pair" "flask_key" {
-  key_name   = "flask-key"
+  key_name = "flask-key"
   # Terraform no expande la tilde (~), por lo que usamos una variable
   # para especificar la ruta del archivo de clave pública.
   public_key = file(var.public_key_path)
 }
 
 resource "aws_instance" "flask_server" {
-  ami           = "ami-0150ccaf51ab55a51" # Amazon Linux 2023 AMI 2023.8.20250707.0 x86_64 HVM kernel-6.1
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public.id
-  key_name      = aws_key_pair.flask_key.key_name
+  ami                    = "ami-0150ccaf51ab55a51" # Amazon Linux 2023 AMI 2023.8.20250707.0 x86_64 HVM kernel-6.1
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public.id
+  key_name               = aws_key_pair.flask_key.key_name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   user_data = <<-EOF
@@ -189,7 +189,7 @@ cd /home/ec2-user
 git clone https://github.com/florencito/fullstack-app-aws-terraform.git
 
 # Ir al directorio de la app Flask
-cd fullstack-app-aws/app
+cd fullstack-app-aws-terraform/app
 
 # Construir y correr el contenedor
 docker build -t flask-app .
